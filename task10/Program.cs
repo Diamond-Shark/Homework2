da79c57b685239d3fb513c464a92784ee4ea7400
D-Shark <yarusia@list.ru>
2022-11-05 21:38:23 +0500

    T10 added

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает 
+// вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int toSecNum = num / 10; 
+int secondNum = toSecNum % 10; 
+
+Console.WriteLine(secondNum);
